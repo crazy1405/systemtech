34c40a88378840a58d4fe69d74c5f70326c0ac3b
crazy1405 <crazy1405@users.noreply.github.com>
2018-08-27 14:04:08 -0500

    primera
    Primera sincronizacion

---
+++ SistemaGimnasio/src/main/java/jorge/gimnasio/controller/PersonaController.java
@@ -0,0 +1,56 @@
+package jorge.gimnasio.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.FacesException;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.imageio.stream.FileImageOutputStream;
+import javax.inject.Inject;
+
+import org.primefaces.event.CaptureEvent;
+
+@ManagedBean
+@ViewScoped
+public class PersonaController {
+	
+	private String filename;
+    
+    private String getRandomImageName() {
+        int i = (int) (Math.random() * 10000000);
+         
+        return String.valueOf(i);
+    }
+ 
+    public String getFilename() {
+        return filename;
+    }
+     
+    public void oncapture(CaptureEvent captureEvent) {
+        filename = getRandomImageName();
+        byte[] data = captureEvent.getData();
+ 
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        String newFileName = externalContext.getRealPath("") + File.separator + "resources" + File.separator + "demo" +
+                                    File.separator + "images" + File.separator + "photocam" + File.separator + filename + ".jpeg";
+         
+        FileImageOutputStream imageOutput;
+        try {
+            imageOutput = new FileImageOutputStream(new File(newFileName));
+            imageOutput.write(data, 0, data.length);
+            imageOutput.close();
+        }
+        catch(IOException e) {
+            throw new FacesException("Error in writing captured image.", e);
+        }
+    }
+	
+	
+	
+}
